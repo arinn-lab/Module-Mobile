2a93b8d88fd8b87f78aa1c3fc153d82dc134ffaa
Arinda Dita Pratiwi <185607584+arinn-lab@users.noreply.github.com>
2026-06-22 20:55:07 +0800

    Delete TugasIndividu10/WheelsUp/LatihanRecyclerView directory

--- TugasIndividu10/WheelsUp/LatihanRecyclerView/app/src/main/java/com/example/latihanrecyclerview/Item.kt
+++
@@ -1,9 +0,0 @@
-package com.example.latihanrecyclerview
-
-data class Item(
-    val id: Int,
-    val title: String,
-    val description: String,
-    val imageRes: Int,
-    var isToggled: Boolean = false
-)

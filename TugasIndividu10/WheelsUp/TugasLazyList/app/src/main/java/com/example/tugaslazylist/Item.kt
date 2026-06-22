5dd0cad9c930f3f129b891612479e4aa5ebcc5d8
Arinda Dita Pratiwi <185607584+arinn-lab@users.noreply.github.com>
2026-06-22 20:56:49 +0800

    Delete TugasIndividu10/WheelsUp/TugasLazyList directory

--- TugasIndividu10/WheelsUp/TugasLazyList/app/src/main/java/com/example/tugaslazylist/Item.kt
+++
@@ -1,9 +0,0 @@
-package com.example.tugaslazylist
-
-data class Item(
-    val id: Int,
-    val title: String,
-    val description: String,
-    val imageRes: Int,
-    var isToggled: Boolean = false
-)
